227340ecc07fb44a86672a28f96e3a729f4509b0
lununglee <leo.lunung.lee@gmail.com>
2020-04-03 12:00:09 -0700

    keeping player within camera bound thru clamp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 {
 	/** Game Configuration */
 	[SerializeField]
-	float	MoveSpeed = 20.0f;
+	float	MovementSpeed = 20.0f;
+
+	[SerializeField]
+	float	BoundaryLimit = 20.0f;
 
 	// Start is called before the first frame update
 	void	Start()
@@ -23,6 +26,9 @@ public class PlayerController : MonoBehaviour
 	void	ProcessInput()
 	{
 		float	HorizontalInput = Input.GetAxis("Horizontal");
-		transform.Translate(Vector3.right * HorizontalInput * MoveSpeed * Time.deltaTime);
+		float	X_Offset = HorizontalInput * MovementSpeed * Time.deltaTime;
+		float	X_UpdatedPos = transform.localPosition.x + X_Offset;
+		X_UpdatedPos = Mathf.Clamp(X_UpdatedPos, -BoundaryLimit, BoundaryLimit);
+		transform.localPosition = new Vector3(X_UpdatedPos, transform.localPosition.y, transform.localPosition.z);
 	}
 }
