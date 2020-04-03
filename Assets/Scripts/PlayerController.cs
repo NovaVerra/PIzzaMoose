fba157703f4f02eae065430a54d487ce0d14acf7
lununglee <leo.lunung.lee@gmail.com>
2020-04-03 11:34:49 -0700

    handle user input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	/** Game Configuration */
+	[SerializeField]
+	float	MoveSpeed = 20.0f;
+
+	// Start is called before the first frame update
+	void	Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void	Update()
+	{
+		ProcessInput();
+	}
+
+	void	ProcessInput()
+	{
+		float	HorizontalInput = Input.GetAxis("Horizontal");
+		transform.Translate(Vector3.right * HorizontalInput * MoveSpeed * Time.deltaTime);
+	}
+}
