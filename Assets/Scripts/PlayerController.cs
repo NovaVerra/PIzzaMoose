1b95f617ad1e9cde789c08791c847791e015845c
lununglee <leo.lunung.lee@gmail.com>
2020-04-22 16:23:48 -0700

    added ObjectPooler; did not implememnt due to bud with engine ver

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,12 +17,6 @@ public class PlayerController : MonoBehaviour
 
 	[SerializeField] float		DestroyDelay = 0.5f;
 
-	// Start is called before the first frame update
-	void	Start()
-	{
-		
-	}
-
 	// Update is called once per frame
 	void	Update()
 	{
